ecd4a872b0876e5ce1590aa1041459990abb909d
jlk50440 <jlk50440@ucmo.edu>
2026-03-24 20:22:29 +0000

    Disable Flask threading for Jenkins lab

--- sample-app.sh
+++ sample-app.sh
@@ -13,15 +13,14 @@ cp -r static/* tempdir/static/.
 
 echo "FROM python:3.11-slim" >> tempdir/Dockerfile
 echo "ENV PIP_PROGRESS_BAR=off" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo 'CMD ["python3","-c","from sample_app import sample; sample.run(host=\"0.0.0.0\", port=5050, threaded=False)"]' >> tempdir/Dockerfile
 echo "EXPOSE 5050" >> tempdir/Dockerfile
-echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 
 docker build -t sampleapp .
-docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker run --privileged --pids-limit=-1 -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a
