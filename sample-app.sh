f546a4e1fb86f79d47a911e602b6cc4f14ca9873
jlk50440 <jlk50440@ucmo.edu>
2026-03-24 20:14:21 +0000

    Fix sample app build script

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf tempdir
+docker rm -f samplerunning 2>/dev/null || true
+
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -8,15 +11,17 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python" >> tempdir/Dockerfile
+echo "FROM python:3.11-slim" >> tempdir/Dockerfile
+echo "ENV PIP_PROGRESS_BAR=off" >> tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
+
 docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
-docker ps -a 
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a
